fe8f07ad40903c74e7b6af7150853711448ba7f3
di07zd4 <charles.walker@tn.gov>
2016-03-14 10:04:59 -0500

    preparing for our initial manual commit - this is for businessLogic.m

---
+++ gitEx7/businessLogic.m
@@ -0,0 +1,13 @@
+//
+//  businessLogic.m
+//  gitEx7
+//
+//  Created by di07zd4 on 3/14/16.
+//  Copyright © 2016 di07zd4. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//  this is just a line for version control to track
+
+
